720b29ffe023f74673530f6a6e3d6e4e16d1035a
Jacob Zapp <jbzapp01@gmail.com>
2026-01-26 11:45:58 -0600

    created camelCase program

---
+++ camelCase.py
@@ -0,0 +1,14 @@
+def camelcase (sentence):
+    title_case = sentence.title() # Uppercase the first letter of each word
+    upper_camel_cased = title_case.replace(' ', '') # Remove spaces between words
+
+    return upper_camel_cased[0:1].lower() + upper_camel_cased[1:]
+
+
+def main():
+    sentence = input('Enter a sentence: ')
+    output = camelcase(sentence)
+    print('CamelCase:', output)
+
+if __name__ == '__main__':
+    main()
