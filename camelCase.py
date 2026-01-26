7de6042deaa5ee8081120b2a306d58b822b560db
Jacob Zapp <jbzapp01@gmail.com>
2026-01-26 11:51:24 -0600

    added banner welcome message at the start of the program

--- camelCase.py
+++ camelCase.py
@@ -4,8 +4,15 @@ def camelcase (sentence):
 
     return upper_camel_cased[0:1].lower() + upper_camel_cased[1:]
 
+def banner():
+    """ Display program name """
+    message = 'CamelCase Converter'
+    stars = '*' * len(message)
+    print(f'{stars} \n{message} \n{stars}\n')
+
 
 def main():
+    banner()
     sentence = input('Enter a sentence: ')
     output = camelcase(sentence)
     print('CamelCase:', output)
